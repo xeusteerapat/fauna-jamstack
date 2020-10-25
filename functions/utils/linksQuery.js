34f18ac6852fbb02b47e029b6eb0c9f07e4e28b9
xeusteerapat <xeus085@gmail.com>
2020-10-25 12:05:31 +0700

    [fix] proper _id

--- functions/utils/linksQuery.js
+++ functions/utils/linksQuery.js
@@ -4,6 +4,7 @@ const GET_LINKS = `
       data{
         _id
         name
+        url
         description
         archived
       }
@@ -11,4 +12,64 @@ const GET_LINKS = `
   }
 `;
 
-module.exports = { GET_LINKS };
+const CREATE_LINK = `
+  mutation(
+    $name: String!
+    $url: String!
+    $description: String!
+  ) {
+    createLink(data: {
+      name: $name,
+      url: $url,
+      description: $description,
+      archived: false
+    }) {
+      _id
+      name
+      url
+      description
+      archived
+    }
+  }
+`;
+
+const UPDATE_LINK = `
+  mutation(
+    $_id: ID!
+    $archived: Boolean!
+    $name: String!
+    $url: String!
+    $description: String!
+  ) {
+    updateLink(
+      id: $_id
+      data: {
+        name: $name
+        url: $url
+        description: $description
+        archived: $archived
+      }
+    ) {
+      name
+      _id
+      url
+      description
+      archived
+    }
+  }
+`;
+
+const DELETE_LINK = `
+  mutation($_id: ID!) {
+    deleteLink(id: $_id) {
+      _id
+    }
+  }
+`;
+
+module.exports = {
+  GET_LINKS,
+  CREATE_LINK,
+  UPDATE_LINK,
+  DELETE_LINK,
+};
