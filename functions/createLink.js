12c685f2e34be4146adc8487d88539562160743f
xeusteerapat <xeus085@gmail.com>
2020-10-25 11:45:25 +0700

    [update] create link handler

---
+++ functions/createLink.js
@@ -0,0 +1,19 @@
+const { CREATE_LINK } = require('./utils/linksQuery');
+const sendQuery = require('./utils/sendQuery');
+const formattedResponse = require('./utils/formattedResponse');
+
+exports.handler = async event => {
+  const { name, url, description } = JSON.parse(event.body);
+  const variables = { name, url, description, archived: false };
+
+  try {
+    const { createLink } = await sendQuery(CREATE_LINK, variables);
+
+    return formattedResponse(200, createLink);
+  } catch (err) {
+    console.log(err);
+    return formattedResponse(500, {
+      error: `Something went wrong with ${err}`,
+    });
+  }
+};
